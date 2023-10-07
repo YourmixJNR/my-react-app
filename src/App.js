74177e838c4b83bfd4380de9a9b9ae33950c037e
Michael Victor <radmakson@gmail.com>
2023-10-07 10:05:01 +0100

    map

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import Dad from './map'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
+    <Dad />
   );
 }
 
---
+++ src/map.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Dad() {
+  // Sample data - an array of items
+  const items = ['Apple', 'Banana', 'Orange', 'Grapes', 'Mango'];
+
+  // Use .map() to create an array of JSX elements
+  const itemElements = items.map((item) => <p>{item}</p>);
+
+  return (
+    <div>
+      <h1>Fruit List</h1>
+      <ul>{itemElements}</ul>
+    </div>
+  );
+}
+
+export default Dad;
