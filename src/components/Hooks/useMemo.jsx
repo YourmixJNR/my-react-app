032fb0fa5a4df1e0da8b11cfe77a4e7635212f6b
Michael Victor <radmakson@gmail.com>
2023-10-17 19:44:20 +0100

    useMemo

--- src/components/Hooks/useMemo.jsx
+++ src/components/Hooks/useMemo.jsx
@@ -1,4 +1,5 @@
 import { useState, useMemo } from "react";
+import todo from "./todo";
 
 const UseMemo = () => {
     const [count, setCount] = useState(0)
@@ -6,9 +7,17 @@ const UseMemo = () => {
 
     const calculation = useMemo(() => expensiveCalculation(count), [count]);
 
+    const addTodo = () => {
+        setCount((t) => [...t, "New Todo"]);
+    }
+
     return (
         <>
-            
+            <h2>My Todos</h2>
+            {todos.map((todo, index) => {
+                return <div key={index}>{todo}</div>;
+            })}
+            <button onClick={addTodo}></button>
         </>
     )
 }
